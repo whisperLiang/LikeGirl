83687bea168968eadf3a978f629c602afbd1cf2e
Pao-Ding-sys <whisperliang11@gmail.com>
2025-11-27 23:20:46 +0800

    Display event image with proper URL handling in lovelist

--- admin/lovelist.php
+++ admin/lovelist.php
@@ -46,7 +46,14 @@ $reslist = mysqli_query($connect, $lovelist);
                             </td>
                             <td><?php echo $list['eventname'] ?></td>
                             <td><?php if ($list['icon']) { ?> <span class="badge badge-success-lighten">已完成 </span><?php } ?><?php if (!$list['icon']) { ?> <span class="badge badge-danger-lighten">未完成</span> <?php } ?></td>
-                            <td><?php if ($list['imgurl']) { ?> <svg t="1718074057742" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6336" width="40" height="40"><path d="M512 512m-512 0a512 512 0 1 0 1024 0 512 512 0 1 0-1024 0Z" fill="#FDEBED" p-id="6337"></path><path d="M642.56 513.28l-140.8 111.36-75.52-60.16L281.6 599.04V371.2c0-28.16 23.04-51.2 51.2-51.2h358.4c28.16 0 51.2 23.04 51.2 51.2v226.56l-99.84-84.48zM371.2 371.2c-21.76 0-38.4 16.64-38.4 38.4s16.64 38.4 38.4 38.4 38.4-16.64 38.4-38.4-16.64-38.4-38.4-38.4z m49.92 220.16l79.36 62.72 142.08-112.64 99.84 88.32V652.8c0 28.16-23.04 51.2-51.2 51.2H332.8c-28.16 0-51.2-23.04-51.2-51.2v-28.16l139.52-33.28z" fill="#EC3A4E" p-id="6338"></path></svg>  <?php } else { ?>暂无图片 <?php } ?></td>
+                            <td><?php if ($list['imgurl']) { 
+                                $imgSrc = $list['imgurl'];
+                                if ($imgSrc && strpos($imgSrc, 'http') !== 0 && strpos($imgSrc, '//') !== 0) {
+                                    $imgSrc = '../' . $imgSrc;
+                                }
+                            ?> 
+                            <img src="<?php echo $imgSrc ?>" style="width: 50px; height: 50px; object-fit: cover; border-radius: 5px;" alt="预览">
+                            <?php } else { ?>暂无图片 <?php } ?></td>
                             <td>
                                 <a href="modlist.php?id=<?php echo $list['id'] ?>&icon=<?php echo $list['icon'] ?>&name=<?php echo $list['eventname'] ?>&imgurl=<?php echo $list['imgurl']; ?> ">
                                     <button type="button" class="btn btn-secondary btn-rounded">
